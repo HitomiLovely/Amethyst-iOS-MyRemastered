3117a187c440bbbf20363e21052bc57bccca14ed
Hitomi <hitomilovely8@gmail.com>
2026-03-08 22:57:36 +0800

    Refactor UISolariumEnabledSafe function
    Removed the weak_import declaration for _UISolariumEnabled and simplified UISolariumEnabledSafe function.
    
    Signed-off-by: Hitomi <hitomilovely8@gmail.com>

--- Natives/UIKit+hook.h
+++ Natives/UIKit+hook.h
@@ -16,11 +16,7 @@ extern NSNotificationName UIPresentationControllerPresentationTransitionWillBegi
 @end
 
 // private functions
-extern BOOL (*_UISolariumEnabled)(void) __attribute__((weak_import));
-
-static inline BOOL UISolariumEnabledSafe(void) {
-    return (_UISolariumEnabled != NULL && _UISolariumEnabled());
-}
+static inline BOOL UISolariumEnabledSafe(void) { return NO; }
 
 @interface UIBarButtonItem(private)
 - (UIView *)view;
